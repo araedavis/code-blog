86313ac18adbe2510978aab3e5d4c56e5a00b65c
Allison Davis <a.rae.davis@gmail.com>
2016-01-07 19:39:02 -0800

    add project data

---
+++ scripts/projects.js
@@ -0,0 +1,44 @@
+var rawProjects = [
+{ title: 'Roller Derby Rules Test',
+  subtitle: 'Bootstrap/vanilla JavaScript ',
+  summary: 'Study aid for new derby players studying for WFTDA rules test.',
+  img: '',
+  projectUrl: 'http://araedavis.com/derbyRulesApp/',
+},
+
+{ title: 'WeatherCat',
+  subtitle: 'forcast.io API/AJAX/jQuery/HTML5 geolocation',
+  summary: 'Displays weather using browser\'s location and forecast.io. A different kitten background for every forecast! Includes toggle between Fahrenheit and Celcius units.',
+  img: '',
+  projectUrl: '',
+},
+
+{ title: 'Calculator',
+  subtitle: 'jQuery/vanilla JavaScript',
+  summary: 'It does the maths',
+  img: '',
+  projectUrl: 'http://codepen.io/davisdavis/full/PZGpaK/',
+},
+
+{ title: 'Work|Play',
+  subtitle: 'jQuery/vanilla JavaScript',
+  summary: 'Pomodoro productivity timer app. Count down work blocks and breaks.',
+  img: '',
+  projectUrl: 'http://codepen.io/davisdavis/full/JYpVEy/',
+},
+
+{ title: 'Writing Sample: Theater Curriculum Guide',
+  subtitle: '',
+  summary: 'Materials distributed to classroom teachers to supplement arts education field trips. Guide format and written content by Allison Davis. ',
+  img: '',
+  projectUrl: 'http://octc.org/pdf/guides/timmyfailure_rg_before.pdf',
+},
+
+{ title: 'Email Campaign Sample: Newsletter',
+  subtitle: '',
+  summary: 'Newsletter for nonprofit outreach network for K-8th grade educators. Content, design, and layout by Allison Davis.',
+  img: '',
+  projectUrl: 'http://us8.campaign-archive2.com/?u=a4da6becf8e6624a9cb4d3ea0&id=bc80528f22&e=[UNIQID]',
+}
+
+];
