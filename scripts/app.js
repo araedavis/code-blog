5686e07f2c12a1cc5cde9b96c446608640b8e2d9
Allison Davis <a.rae.davis@gmail.com>
2016-01-09 13:57:54 -0800

    append projects to #projects section

--- scripts/app.js
+++ scripts/app.js
@@ -11,18 +11,23 @@ Project.prototype.toHtml = function(){
 
   //find each element of clone, populate with the property to be placed there
   $newProject.find('h1').html(this.title);
+  //add additional elements & properties here
 
   return $newProject;
 
 };
 
-console.log(rawProjects);
+//pushes each rawProject object to projects arr as a new Project object
 rawProjects.forEach(function(el){
   projects.push(new Project(el));
 
 });
 
 console.log(projects);
+
+projects.forEach(function(pro){
+  $('#projects').append(pro.toHtml());
+});
 
 //Next up: basic css styles for #projects
 //Next up: contain projects in a carousel or other slightly more interactive display
