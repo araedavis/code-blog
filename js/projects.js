9efe50b5d54b3ce35e619770611ff091a04fddc3
Allison Davis <a.rae.davis@gmail.com>
2016-01-09 14:16:07 -0800

    add placeholder html for styling

--- js/projects.js
+++ js/projects.js
@@ -1,12 +1,50 @@
 var rawProjects = [
-  {title: '',
+{ title: 'Roller Derby Rules Test',
+  subtitle: 'Bootstrap/vanilla JavaScript',
+  summary: 'Study aid for new derby players studying for WFTDA rules test.',
+  img: 'imgs/derby_thumb.jpg',
+  projectUrl: 'http://araedavis.com/derbyRulesApp/',
+  dateCreated: '',
+},
+
+{ title: 'WeatherCat',
+  subtitle: 'forcast.io API/AJAX/jQuery/HTML5 geolocation',
+  summary: 'Displays weather using browser\'s location and forecast.io. A different kitten background for every forecast! Includes toggle between Fahrenheit and Celcius units.',
+  img: 'imgs/WeatherCat_thumb.jpg',
+  projectUrl: 'http://codepen.io/davisdavis/full/qOREMo/',
+  dateCreated: '',
+},
+
+{ title: 'Calculator',
+  subtitle: 'jQuery/vanilla JavaScript',
+  summary: 'It does the maths',
+  img: 'imgs/placeholder.jpg',
+  projectUrl: 'http://codepen.io/davisdavis/full/PZGpaK/',
+  dateCreated: '',
+},
+
+{ title: 'Work|Play',
+  subtitle: 'jQuery/vanilla JavaScript',
+  summary: 'Pomodoro productivity timer app. Count down work blocks and breaks.',
+  img: 'imgs/placeholder.jpg',
+  projectUrl: 'http://codepen.io/davisdavis/full/JYpVEy/',
+  dateCreated: '',
+},
+
+{ title: 'Writing Sample: Theater Curriculum Guide',
   subtitle: '',
-  summary: '',
-  img: '',
-  
-  }
+  summary: 'Materials distributed to classroom teachers to supplement arts education field trips. Guide format and written content by Allison Davis. ',
+  img: 'OCT_resourceGuide.png',
+  projectUrl: 'http://octc.org/pdf/guides/timmyfailure_rg_before.pdf',
+  dateCreated: '',
+},
 
-
-
+{ title: 'Email Campaign Sample: Newsletter',
+  subtitle: '',
+  summary: 'Newsletter for nonprofit outreach network for K-8th grade educators. Content, design, and layout by Allison Davis.',
+  img: 'OCT_emailThumb.png',
+  projectUrl: 'http://us8.campaign-archive2.com/?u=a4da6becf8e6624a9cb4d3ea0&id=bc80528f22&e=[UNIQID]',
+  dateCreated: '',
+}
 
 ];
