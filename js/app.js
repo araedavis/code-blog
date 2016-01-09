821577fdf12c5fe3806e8d373c565381a97c9adc
Allison Davis <a.rae.davis@gmail.com>
2016-01-09 15:28:01 -0800

    fix duplicate project cloning bug

--- js/app.js
+++ js/app.js
@@ -7,11 +7,13 @@ function Project(opts){
 }
 
 Project.prototype.toHtml = function(){
-  var $newProject = $('.project').clone();
+  var $newProject = $('.template').clone();
 
   //find each element of clone, populate with the property to be placed there
   $newProject.find('h1').html(this.title);
   //add additional elements & properties here
+
+  $newProject.removeClass('template');
 
   return $newProject;
 
@@ -19,16 +21,16 @@ Project.prototype.toHtml = function(){
 
 //pushes each rawProject object to projects arr as a new Project object
 
-// rawProjects.forEach(function(el){
-//   projects.push(new Project(el));
-//
-// });
-//
-// console.log(projects);
-//
-// projects.forEach(function(pro){
-//   $('#projects').append(pro.toHtml());
-// });
+rawProjects.forEach(function(el){
+  projects.push(new Project(el));
+
+});
+
+console.log(projects);
+
+projects.forEach(function(pro){
+  $('#projects').append(pro.toHtml());
+});
 
 //Next up: basic css styles for #projects
 //Next up: contain projects in a carousel or other slightly more interactive display
--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,11 @@
 $(function(){
   //hide .main-nav
-  $('.main-nav').hide();
+  $('.main-nav').hide(); //this will change to an addClass()
 
-  $('.hamburger').on('click', function(){
+  $('.hamburger').on('click', function(e){
+    e.preventDefault();
     $('.main-nav').show();
   });
-  //next up: rehide when menu icon is reclicked
+  //next up: rehide when menu icon is reclicked (using show/hide class?)
 
 });
