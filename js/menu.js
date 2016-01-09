c6c3dc2409cb1cba56fc3bd7449e2780d81ad5f1
Allison Davis <a.rae.davis@gmail.com>
2016-01-09 15:01:27 -0800

    add hamburger menu

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+$(function(){
+  //hide .main-nav
+  $('.main-nav').hide();
+
+  $('.hamburger').on('click', function(){
+    $('.main-nav').show();
+  });
+  //next up: rehide when menu icon is reclicked
+
+});
