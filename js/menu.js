ac6ac839007d544c876bb9a1ce90e0468366e80d
Allison Davis <a.rae.davis@gmail.com>
2016-01-14 21:24:05 -0800

    add show/hide functionality for project summary; add dynamic +/- icon set

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,4 @@
+//nav menu function
 function hideNav(){
   if ($('.main-nav').hasClass('hidden')){
     $('.main-nav').css('visibility', 'hidden');
@@ -6,10 +7,35 @@ function hideNav(){
   }
 };
 
-collapseNav();
+//hideNav();
 
 $('.hamburger').on('click', function(e){
   e.preventDefault();
   $('.main-nav').toggleClass('hidden');
   collapseNav();
+});
+
+//Accordian function
+
+function hideProjectSummary(){
+  if($('.accordian').hasClass('hide') === true){
+    $('.summary').hide();
+  } else {
+    $('.summary').show();
+  }
+}
+
+hideProjectSummary();
+
+$('.accordian').on('click', function(e){
+  e.preventDefault();
+  $('.accordian').toggleClass('hide');
+
+  if($('.accordian').hasClass('hide') === false){
+    $('.accordian > i').addClass('fa-minus-circle').removeClass('fa-plus-circle');
+  } else {
+    $('.accordian > i').addClass('fa-plus-circle').removeClass('fa-minus-circle');
+  }
+
+  hideProjectSummary();
 });
