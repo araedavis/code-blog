b3cecde516a05786c915deb40a6b5c22b91813e5
Doug Popadince <gsmatth@icloud.com>
2016-01-30 11:21:59 -0800

    added iffes to view and project js files

--- js/app.js
+++ js/app.js
@@ -1,56 +1,59 @@
 
 //create a constructor function for project objects
-function Project(opts){
-  this.title = opts.title;
-  this.subtitle = opts.subtitle;
-  this.summary = opts.summary;
-  this.img = opts.img;
-  this.projectUrl = opts.projectUrl;
-  this.dateCreated = opts.dateCreated;
-  this.category = opts.category;
-}
+(function(module) {
+  function Project(opts){
+    this.title = opts.title;
+    this.subtitle = opts.subtitle;
+    this.summary = opts.summary;
+    this.img = opts.img;
+    this.projectUrl = opts.projectUrl;
+    this.dateCreated = opts.dateCreated;
+    this.category = opts.category;
+  }
 
-Project.all = [];
+  Project.all = [];
 
-Project.prototype.toHtml = function(){
-  var source = $('#projectTemplate').html();
-  var template = Handlebars.compile(source);
+  Project.prototype.toHtml = function(){
+    var source = $('#projectTemplate').html();
+    var template = Handlebars.compile(source);
 
-  var context = {
-    title: this.title,
-    subtitle: this.subtitle,
-    summary: this.summary,
-    img: this.img,
-    projectUrl: this.projectUrl,
-    category: this.category
+    var context = {
+      title: this.title,
+      subtitle: this.subtitle,
+      summary: this.summary,
+      img: this.img,
+      projectUrl: this.projectUrl,
+      category: this.category
+    };
+
+    var html = template(context);
+    return html;
   };
 
-  var html = template(context);
-  return html;
-};
-
-Project.loadAll = function(rawProjects){
-//pushes each rawProject object to Project.all
-  rawProjects.forEach(function(el){
-    Project.all.push(new Project(el));
-  });
-
-//renders each project using toHtml method
-  Project.all.forEach(function(pro){
-    $('.project-container').append(pro.toHtml());
-  });
-};
-
-Project.fetchAll = function(){
-  //if project data exists in local storage
-  if(localStorage.rawProjects){
-    Project.loadAll(JSON.parse(localStorage.rawProjects));
-  } else {
-    //else ajax!
-    $.get('data/projects.json', function(data){
-      Project.loadAll(data);
-      var dataString = JSON.stringify(data);
-      localStorage.setItem('rawProjects', dataString);
+  Project.loadAll = function(rawProjects){
+  //pushes each rawProject object to Project.all
+    rawProjects.forEach(function(el){
+      Project.all.push(new Project(el));
     });
-  }
-};
+
+  //renders each project using toHtml method
+    Project.all.forEach(function(pro){
+      $('.project-container').append(pro.toHtml());
+    });
+  };
+
+  Project.fetchAll = function(){
+    //if project data exists in local storage
+    if(localStorage.rawProjects){
+      Project.loadAll(JSON.parse(localStorage.rawProjects));
+    } else {
+      //else ajax!
+      $.get('data/projects.json', function(data){
+        Project.loadAll(data);
+        var dataString = JSON.stringify(data);
+        localStorage.setItem('rawProjects', dataString);
+      });
+    }
+  };
+  module.Project = Project;
+})(window);
--- js/projectView.js
+++ js/projectView.js
@@ -1,77 +1,81 @@
-var projectView = {};
+(function(module){
+  var projectView = {};
 
-//DONE: create filters/categories for projects: ie, javascript, writing, marketing, etc.
-projectView.populateCategoryFilter = function(){
-  var filterVal = [];
-  $('.project').each(function(){
-    var val = $(this).attr('data-category');
-    filterVal.push(val);
-
-    filterVal = $.unique(filterVal);
-    return filterVal;
-  });
-
-  $(filterVal).each(function(num, val){
-    var optionTag = '<option value="' + val + '">' + val + '</option>';
-    $('#category-filter').append(optionTag);
-  });
-};
-
-projectView.handleCategoryFilter = function(){
-  $('#category-filter').on('change', function(){
-    var filtered = $(this).val();
-
+  //DONE: create filters/categories for projects: ie, javascript, writing, marketing, etc.
+  projectView.populateCategoryFilter = function(){
+    var filterVal = [];
     $('.project').each(function(){
-      $(this).hide();
+      var val = $(this).attr('data-category');
+      filterVal.push(val);
 
-      if($(this).data('category') === filtered){
-        $(this).show();
-      } else if (filtered === 'Filter by Category' || filtered === ''){
-        $(this).show();
-      };
+      filterVal = $.unique(filterVal);
+      return filterVal;
     });
 
+    $(filterVal).each(function(num, val){
+      var optionTag = '<option value="' + val + '">' + val + '</option>';
+      $('#category-filter').append(optionTag);
+    });
+  };
+
+  projectView.handleCategoryFilter = function(){
+    $('#category-filter').on('change', function(){
+      var filtered = $(this).val();
+
+      $('.project').each(function(){
+        $(this).hide();
+
+        if($(this).data('category') === filtered){
+          $(this).show();
+        } else if (filtered === 'Filter by Category' || filtered === ''){
+          $(this).show();
+        };
+      });
+
+    });
+  };
+
+  projectView.handleTabs = function(){
+    $('#projects').hide();
+    $('#about').hide();
+
+    $('.tab').on('click','a', function(){
+      $('section').hide();
+      $('#landing').hide();
+      var tabId = $(this).data('tab');
+      $('#' + tabId).show();
+
+    });
+  };
+
+  projectView.toggleSummary = function(){
+    $('.summary').hide();
+
+    $('.accordian').on('click', function(e){
+      e.preventDefault();
+      var target = $(e.target).parent().parent().next();  //target the project summary
+      $(e.target).toggleClass('fa-minus-circle fa-plus-circle');
+
+      if($('.accordian > i').hasClass('fa-minus-circle') === true){
+        $(target).show();
+      } else {
+        $(target).hide();
+      }
+    });
+  };
+
+  //wraps every 2 projects into a row div for skeleton grid layout
+  projectView.skeletonGrid = function (){
+    $('.container:nth-child(2)').wrap('<div class="row"></div>');
+  };
+
+  $(function(){
+    projectView.skeletonGrid();
+    projectView.populateCategoryFilter();
+    projectView.handleCategoryFilter();
+    projectView.handleTabs();
+    projectView.toggleSummary();
   });
-};
 
-projectView.handleTabs = function(){
-  $('#projects').hide();
-  $('#about').hide();
-
-  $('.tab').on('click','a', function(){
-    $('section').hide();
-    $('#landing').hide();
-    var tabId = $(this).data('tab');
-    $('#' + tabId).show();
-
-  });
-};
-
-projectView.toggleSummary = function(){
-  $('.summary').hide();
-
-  $('.accordian').on('click', function(e){
-    e.preventDefault();
-    var target = $(e.target).parent().parent().next();  //target the project summary
-    $(e.target).toggleClass('fa-minus-circle fa-plus-circle');
-
-    if($('.accordian > i').hasClass('fa-minus-circle') === true){
-      $(target).show();
-    } else {
-      $(target).hide();
-    }
-  });
-};
-
-//wraps every 2 projects into a row div for skeleton grid layout
-projectView.skeletonGrid = function (){
-  $('.container:nth-child(2)').wrap('<div class="row"></div>');
-};
-
-$(function(){
-  projectView.skeletonGrid();
-  projectView.populateCategoryFilter();
-  projectView.handleCategoryFilter();
-  projectView.handleTabs();
-  projectView.toggleSummary();
-});
+  module.projectView = projectView;
+})(window);
