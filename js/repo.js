92ab8e0344c28fc626e9c946e8ae5a405894b819
Allison Davis <a.rae.davis@gmail.com>
2016-02-04 20:54:08 -0800

    repo.fetch now working

--- js/repo.js
+++ js/repo.js
@@ -5,10 +5,13 @@
 
   repo.fetch = function(callback){
     $.ajax({
-      url: 'https://api.github.com/users/araedavis/repos',
+      url: 'https://api.github.com/users/araedavis/repos', //+ '?callback=callback&access_token=' + githubToken,
       type: 'GET',
-      headers: { 'Authorization': + 'token ' + githubToken },
-      success: function(data){
+      //error: function(xhr){
+      //   console.log(xhr);
+      // },
+      headers: { 'Authorization': 'token ' + githubToken },
+      success: function(data, status, xhr){
         repo.all = data;
         console.log(repo.all);
         callback();
