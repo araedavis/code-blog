e1c6b9ccf9433d4634f2ed83ff39ed34e2109082
Allison Davis <a.rae.davis@gmail.com>
2016-02-09 20:07:54 -0800

    update url for repo.js

--- js/repo.js
+++ js/repo.js
@@ -6,12 +6,11 @@
 
   repo.fetch = function(callback){
     $.ajax({
-      url: 'https://api.github.com/users/araedavis/repos',
+      url: 'github.com/users/araedavis/repos',
       type: 'GET',
       //error: function(xhr){
       //   console.log(xhr);
       // },
-      headers: { 'Authorization': 'token ' + githubToken },
       success: function(data, status, xhr){
         console.log(data);
         repo.all = data;
