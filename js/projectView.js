dc7275d0584323e3d2256e3f82321e33c04db733
Allison Davis <a.rae.davis@gmail.com>
2016-02-10 18:21:51 -0800

    fix mobile nav hide

--- js/projectView.js
+++ js/projectView.js
@@ -88,11 +88,20 @@
 
   projectView.handleNav = function(){
     $('.hamburger').off('click');
+
     $('.hamburger').on('click', function(e){
       e.preventDefault();
       $('.main-nav').slideToggle('600', function(){
         $('.hamburger > i').toggleClass('fa-times fa-bars');
       });
+    });
+
+    $(window).on('resize', function(){
+      if($(window).width() >= 768){
+        $('.main-nav').show();
+      } else if($(window).width() < 768){
+        $('.main-nav').hide();
+      };
     });
   };
 
